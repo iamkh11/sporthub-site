d1b2b7da6ca3ba51a0de1996712abcc1b775d19d
iamkh11 <iamkh11@outlook.com>
2021-07-02 01:49:48 +0100

    xDD

--- src/main/java/com/kh/Webpfe/controllers/MatchController.java
+++ src/main/java/com/kh/Webpfe/controllers/MatchController.java
@@ -1,12 +1,20 @@
 package com.kh.Webpfe.controllers;
 
 
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kh.Webpfe.repositories.MatchReposiroty;
+import com.kh.Webpfe.repositories.TicketRepository;
+import com.kh.Webpfe.helpers.ZXingHelper;
 
 
 
@@ -18,7 +26,8 @@ public class MatchController {
 	@Autowired
 	private MatchReposiroty matchRepository;
 	
-	
+	@Autowired
+	private TicketRepository ticketRepository;
 	
 	
 	
@@ -49,6 +58,28 @@ public class MatchController {
 		       
 		        return "fixtures-hand";
 		    }
+		  
+		  
+		  
+		  
+		  
+		  
+		  @RequestMapping("/buy-ticket")
+		    public String ticket(Model model) {
+				
+		        model.addAttribute("match", matchRepository.findAll());
+		        model.addAttribute("ticket", ticketRepository.findAll());
+		        return "buy-ticket";
+		    }
+
+			@RequestMapping(value = "qrcode/{match_tag}", method = RequestMethod.GET)
+			public void qrcode(@PathVariable("match_tag") String match_tag, HttpServletResponse response) throws Exception {
+				response.setContentType("image/png");
+				OutputStream outputStream = response.getOutputStream();
+				outputStream.write(ZXingHelper.getQRCodeImage(match_tag, 500, 500));
+				outputStream.flush();
+				outputStream.close();
+			}
 		
 	
 	
